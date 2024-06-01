0f82e73b02b2ea962dbc3926a3069607fc79aaa4
ChrisLubin <chrisx700@gmail.com>
2024-06-01 18:30:12 -0400

    Make motors stop when angle threshold is exceeded

--- self-balancing-robot/self-balancing-robot/Accelerometer.h
+++ self-balancing-robot/self-balancing-robot/Accelerometer.h
@@ -3,6 +3,7 @@
 #include <PID_v1.h>
 
 #define DATA_CHECK_INTERVAL_MILLIS 10
+#define STOP_MOTORS_ANGLE_THRESHOLD 45
 
 MPU6050 mpu(Wire);
 
@@ -16,6 +17,8 @@ double pidOutput;
 double kp = 6;
 double ki = 1300;
 double kd = 0.07;
+const double STOP_MOTORS_LOW_ANGLE_THRESHOLD = targetAngle - STOP_MOTORS_ANGLE_THRESHOLD;
+const double STOP_MOTORS_HIGH_ANGLE_THRESHOLD = targetAngle + STOP_MOTORS_ANGLE_THRESHOLD;
 PID pidController(&currentAngle, &pidOutput, &targetAngle, kp, ki, kd, REVERSE);
 
 void setUpAccelerometer() {
@@ -47,16 +50,22 @@ void accelerometerLoop() {
   }
 }
 
+int getMotorSpeedFromAccelerometer() {
+  if (currentAngle < STOP_MOTORS_LOW_ANGLE_THRESHOLD || currentAngle > STOP_MOTORS_HIGH_ANGLE_THRESHOLD) {
+    return 0;
+  }
+
+  return constrain(abs(pidOutput), LOWEST_MOTOR_SPEED, HIGHEST_MOTOR_SPEED);
+}
+
+MOTOR_ACTIONS getMotorActionFromAccelerometer() {
+  return pidOutput > 0 ? GO_FORWARD : GO_BACKWARD;
+}
+
 MotorCommand getCommandFromAccelerometer() {
   MotorCommand command;
-  double motorSpeedToUse = abs(pidOutput);
-  command.speed = constrain(motorSpeedToUse, LOWEST_MOTOR_SPEED, HIGHEST_MOTOR_SPEED);
-
-  if (pidOutput > 0) {
-    command.action = GO_FORWARD;
-  } else {
-    command.action = GO_BACKWARD;
-  }
+  command.speed = getMotorSpeedFromAccelerometer();
+  command.action = getMotorActionFromAccelerometer();
 
   return command;
 }
